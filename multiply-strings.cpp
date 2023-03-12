484cedc6d68d12ab05929672102de91cd76e153d
tastycrayon <mosiur.404.rahman@gmail.com>
2023-03-13 04:11:01 +0600

    multiply-strings

---
+++ multiply-strings.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#define log(x) std::cout << x << std ::endl;
+using namespace std;
+
+// num1 : "123", num2 : "456", "56088",
+//   123
+//   456
+//   738
+//  615x
+// 492xx
+// 56088
+
+class Solution {
+ public:
+  string multiply(string num1, string num2) {
+    const char zero = '0';
+    if (num1.length() == 1 && num1[0] == zero) return "0";
+    if (num2.length() == 1 && num2[0] == zero) return "0";
+
+    int maxLength = num1.length() + num2.length();
+
+    vector<int8_t> result = vector<int8_t>(maxLength, 0);
+    int resultIndex = maxLength - 2;
+
+    for (int i = num2.length() - 1; i >= 0; i--) {
+      int digit2 = num2[i] - zero;
+      for (int j = num1.length() - 1; j >= 0; j--) {
+        int digit1 = num1[j] - zero, carry = 0;
+        int temp = digit1 * digit2 + carry;
+        int r = temp % 10;
+        carry = temp / 10;
+        result[resultIndex - i - j] += r;  // overflow possible
+
+        if (result[resultIndex - i - j] > 9) {
+          carry += result[resultIndex - i - j] / 10;
+          result[resultIndex - i - j] %= 10;
+        }
+
+        result[resultIndex - i - j + 1] += carry;  // carry
+      }
+    }
+    string strResult;
+    strResult.reserve(maxLength);
+    bool checkForZero = 1;
+    for (int i = result.size() - 1; i >= 0; i--) {
+      if (result[i] != 0) checkForZero = false;
+      if (checkForZero) continue;
+      strResult.push_back(result[i] + zero);  // reversing
+    }
+    return strResult;
+  }
+};
+
+struct CaseType {
+  string num1;
+  string num2;
+  string result;
+};
+int main(int argc, char const *argv[]) {
+  CaseType cases[] = {
+      {
+        num1 : "2",
+        num2 : "13",
+        result : "26",
+      },
+      {
+        num1 : "123",
+        num2 : "456",
+        result : "56088",
+      },
+      {
+        num1 : "9",
+        num2 : "9",
+        result : "81",
+      },
+      {
+        num1 : "925101087184894",
+        num2 : "3896737933784656127",
+        result : "3604876499018802870538090258945538",
+      },
+  };
+  string num1 = "hello";
+  string num2 = "ll";
+
+  for (int i = 0; i < 4; i++) {
+    string result = Solution().multiply(cases->num1, cases->num2);
+    cout << (result == cases->result) << endl;
+  }
+
+  return 0;
+}
